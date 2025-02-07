cd7d11906050df9dffbad3f9b4c61612ea508c53
tristan-wright <tristan.andrew.wright@gmail.com>
2025-02-07 13:16:04 +0930

    Added some better handling and arithmatic to first showcase

--- showcase_1/main.go
+++ showcase_1/main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
 )
 
 func main() {
+
+	var result float32
+
+	operatorPtr := flag.String("operator", "add", "A string to either add, subtract, multiply or divide")
+	flag.Parse()
+
 	args := os.Args[1:]
 
-	if len(args) != 2 {
-		fmt.Println("Need 2 arguments")
+	if len(args) != 3 {
+		fmt.Println("Need 3 arguments\n" +
+			"In the format of number operand number. E.g. 4 times 5")
 		os.Exit(1)
 	}
 
-	firstVal, err := strconv.Atoi(args[0])
+	firstVal, err := strconv.Atoi(args[1])
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	secondVal, err := strconv.Atoi(args[1])
+	secondVal, err := strconv.Atoi(args[2])
 
 	if err != nil {
 		fmt.Println(err)
@@ -33,11 +42,52 @@ func main() {
 		os.Exit(1)
 	}
 
-	result, remainder := calculate(firstVal, secondVal)
+	err = check_operator(operatorPtr)
 
-	fmt.Println("Result:", result, "Remainder:", remainder)
+	if err != nil {
+		fmt.Println("Incorrect operator. See help.")
+	}
+
+	switch *operatorPtr {
+	case "add":
+		result = add(firstVal, secondVal)
+
+	case "divide":
+		result = divide(firstVal, secondVal)
+
+	case "multiply":
+		result = multiply(firstVal, secondVal)
+
+	case "subtract":
+		result = subtract(firstVal, secondVal)
+	}
+
+	fmt.Println("Result:", result)
 }
 
-func calculate(val1 int, val2 int) (int, int) {
-	return val1 / val2, val1 % val2
+func check_operator(operator *string) error {
+	valid_operators := [4]string{"add", "divide", "multiply", "subtract"}
+
+	for _, valid_operator := range valid_operators {
+		if *operator == valid_operator {
+			return nil
+		}
+	}
+	return errors.New("incorrect operator")
+}
+
+func add(val1 int, val2 int) float32 {
+	return float32(val1) + float32(val2)
+}
+
+func divide(val1 int, val2 int) float32 {
+	return float32(val1) / float32(val2)
+}
+
+func multiply(val1 int, val2 int) float32 {
+	return float32(val1) * float32(val2)
+}
+
+func subtract(val1 int, val2 int) float32 {
+	return float32(val1) - float32(val2)
 }
